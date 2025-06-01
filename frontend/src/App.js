46793c0802d1895b41d43eb2e9b501030857c1cf
Maya Lee <maya.v.lee393@gmail.com>
2025-06-01 00:24:35 -0500

    basic nav

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,43 +4,25 @@ import './App.css';
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min";
 
+import {
+    BrowserRouter as Router,
+    Routes,
+    Route,
+} from "react-router-dom";
+
+import Login from './pages/Login';
+import Chat from './pages/Chat';
+
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <h1 className="text-3xl font-bold underline">
-      Hello world! with tailwind
-    </h1>
-    <div className="bg-info p-5">
-                <p className="">
-                    This is example for using button with
-                    bootstrap styling
-                </p>
-                <a
-                    className="btn btn-primary"
-                    data-bs-toggle="collapse"
-                    href="#collapseExample"
-                    role="button"
-                    aria-expanded="false"
-                    aria-controls="collapseExample"
-                >
-                    Bootstrap button
-                </a>
-            </div>
-      </header>
-      
+      <Router>
+        <Routes>
+          <Route exact path="/" element={<Login />} />
+          <Route path="/chat" element={<Chat />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
