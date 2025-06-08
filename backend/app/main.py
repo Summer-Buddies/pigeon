4c49088b300667ddb5aaeff091b3670554b297ca
Kevin Taing <vintaing@gmail.com>
2025-06-08 17:27:05 -0500

    added venv to gitignore, added some cmds to dockerfile, added socket support for main.py, updated imports in requirement.txt

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,12 +1,17 @@
 from fastapi import FastAPI
 from dotenv import load_dotenv
+from app.api.routes import router as api_router
+from app.socket.manager import ws_router
 import os
 
 app = FastAPI()
 
 load_dotenv()  # take environment variables from .env.
 
-DATABASE_URL = os.getenv("DATABASE_URL")
+FIREBASE_KEY = os.getenv("FIREBASE_KEY")
+
+app.include_router(api_router)
+app.include_router(ws_router)
 
 @app.get("/")
 def read_root():
