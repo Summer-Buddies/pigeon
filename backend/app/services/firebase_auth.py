133e726cfe7ec47a5a64952e9fd975ffb93ca26a
Kevin Taing <vintaing@gmail.com>
2025-06-10 13:10:57 -0500

    added skeleton code for firebase_auth.py

--- backend/app/services/firebase_auth.py
+++ backend/app/services/firebase_auth.py
@@ -1,1 +1,13 @@
-# firebase_auth.py
+import firebase_admin
+from firebase_admin import credentials, auth
+from fastapi import HTTPException
+
+cred = credentials.Certificate("app/firebase-adminsdk.json")
+firebase_admin.initialize_app(cred)
+
+def verify_firebase_token(id_token: str):
+    try:
+        decoded_token = auth.verify_id_token(id_token)
+        return decoded_token
+    except Exception:
+        raise HTTPException(status_code = 401, detail = "Invalid Firebase token")
