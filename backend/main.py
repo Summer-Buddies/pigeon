7ec65eb7bd1bcf3bbc766dd2240ecbe7a943c5ec
Amy Wu <amypwu@utexas.edu>
2025-06-12 22:21:32 +0000

    fixed .env credential load errors & check if firebase alr initialized

--- backend/main.py
+++ backend/main.py
@@ -1,16 +1,31 @@
+
 from fastapi import FastAPI, Request, Depends, HTTPException, status
 import firebase_admin
 from firebase_admin import credentials, auth as firebase_auth
+
 from dotenv import load_dotenv
+load_dotenv()  # take environment variables from .env.
+
 from app.api.routes import router as api_router
 from app.socket.manager import ws_router
 import os
 
 app = FastAPI()
 
-load_dotenv()  # take environment variables from .env.
+cred = credentials.Certificate({
+    "type": "service_account",
+    "private_key": os.getenv("FIREBASE_PRIVATE_KEY").replace('\\n', '\n'),
+    "private_key_id": os.getenv("FIREBASE_PRIVATE_KEY_ID"),
+    "client_email": os.getenv("FIREBASE_CLIENT_EMAIL"),
+    "client_id": os.getenv("FIREBASE_CLIENT_ID"),
+    "auth_uri": os.getenv("FIREBASE_AUTH_URI"),
+    "token_uri": os.getenv("FIREBASE_TOKEN_URI"),
+    "auth_provider_x509_cert_url": os.getenv("FIREBASE_AUTH_PROVIDER_CERT_URL"),
+    "client_x509_cert_url": os.getenv("FIREBASE_CLIENT_CERT_URL")
+})
 
-FIREBASE_PRIVATE_KEY = os.getenv("FIREBASE_PRIVATE_KEY")
+if not firebase_admin._apps: #dictionary that stores all initialized Firebase apps
+    firebase_admin.initialize_app(cred)
 
 app.include_router(api_router)
 app.include_router(ws_router)
