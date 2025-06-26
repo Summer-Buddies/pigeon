da0dc090b8ee81e76c6daede513a7e761dfb301e
Alana Koshikawa <alanakoshikawa@gmail.com>
2025-06-26 12:35:09 -0500

    static friend system

--- frontend/src/components/SidePanel.js
+++ frontend/src/components/SidePanel.js
@@ -1,5 +1,6 @@
 import { useNavigate } from 'react-router-dom';
 import { HiPlus } from 'react-icons/hi'
+import { friends } from '../data/staticUsers';
 import './SidePanel.css'
 
 const SidePanel = () => {
@@ -11,7 +12,7 @@ const SidePanel = () => {
                 <h1>Pigeon</h1>
                 <div className="side-options">
                     <div className='search'>
-                        <img src='search.png' alt='search' className='search-icon'></img>
+                        <img src='/search.png' alt='search' className='search-icon'></img>
                         <input
                             type='text'
                             placeholder='Search'  
@@ -19,8 +20,8 @@ const SidePanel = () => {
                         ></input>
                     </div>
                     <div className='friends'>
-                        <h2>Friends</h2>
-                        <img src='users.png' alt='friends' className='friends-icon'/>
+                        <h2 onClick={() => navigate('/friends')}>Friends</h2>
+                        <img src='/users.png' alt='friends' className='friends-icon'/>
                     </div>
                     <div className='messages'>
                         <h2>Messages</h2>
@@ -28,15 +29,25 @@ const SidePanel = () => {
                     </div>
                 </div>
             </div>
-            <div className='user'>
-                <div className='profile-pic'>
-
-                </div>
-                <div className='name'>
-                    <h3>Hammie</h3>
-                </div>
-                <div className='settings'>
-                    <img src='settings.png' alt='settings' onClick={() => navigate('/settings')}/>
+            <div className='users'>
+                {friends.map(user => (
+                    <div key={user.id} className="user" onClick={() => navigate(`/chat/${user.id}`)}>
+                            <div className="profile-pic" />
+                        <div className="name">
+                            <h3>{user.name}</h3>
+                        </div>
+                    </div>
+                ))}
+            </div>
+            <div className='user-profile'>
+                <div className='user'>
+                    <div className='profile-pic'/>
+                    <div className='name'>
+                        <h3>Hammie</h3>
+                    </div>
+                    <div className='settings'>
+                        <img src='/settings.png' alt='settings' onClick={() => navigate('/settings')}/>
+                    </div>
                 </div>
             </div>
         </div>
---
+++ frontend/src/data/staticUsers.js
@@ -0,0 +1,14 @@
+export const friends = [
+    {id: 1, name: 'Pochacco'},
+    {id: 2, name: 'Miffy'},
+    {id: 3, name: 'Totoro'}
+];
+
+export const friendRequests = [
+    {id: 4, name: 'Lune'},
+    {id: 5, name: 'Mario'}
+]
+
+export const requestingFriends = [
+    {id: 6, name: 'RUMI'}
+]
